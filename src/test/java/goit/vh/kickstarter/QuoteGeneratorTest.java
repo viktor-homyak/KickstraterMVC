eec5885087d7ad1e4987d59fe3e3c6f0fdb3f56a
Vityunya <h.vitya56@gmail.com>
2015-07-26 14:12:25 +0300

    commit

---
+++ src/test/java/goit/vh/kickstarter/QuoteGeneratorTest.java
@@ -0,0 +1,18 @@
+package goit.vh.kickstarter;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Viktor on 26.07.2015.
+ */
+public class QuoteGeneratorTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void testGetQuote() throws Exception {
+
+    }
+}
